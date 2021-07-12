87d4ff640250901f0287950aaed46ab5e7113980
koheisanno <kohei.sanno@gmail.com>
2021-07-12 21:37:57 +0900

    static config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
   // on Windows you might want to set publicPath: "http://127.0.0.1:8080/"
   publicPath: "/static/",
   outputDir: "./dist/",
+  assetsDir: "/static/",
 
   chainWebpack: (config) => {
     config
