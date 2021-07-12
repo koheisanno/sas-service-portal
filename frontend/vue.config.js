1c18f1ffdc031e558495876255f83fc2b4cf5680
koheisanno <kohei.sanno@gmail.com>
2021-07-12 21:24:32 +0900

    settings

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,8 +2,8 @@ const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
   // on Windows you might want to set publicPath: "http://127.0.0.1:8080/"
-  publicPath: "/static/",
   outputDir: "./dist/",
+  assetsDir: 'static',
 
   chainWebpack: (config) => {
     config
