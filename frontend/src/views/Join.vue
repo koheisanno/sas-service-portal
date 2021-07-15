8c012c6bc78cecdadb8568bda17322f9ec4329af
koheisanno <kohei.sanno@gmail.com>
2021-07-15 23:59:09 +0900

    join style

--- frontend/src/views/Join.vue
+++ frontend/src/views/Join.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    <h1 class="py-3">Welcome! You just joined {{ name }}</h1>
-    <h2 class="py-3">Now redirecting you to the home page...</h2>
+  <div v-if="loading" class="d-flex justify-content-center">
+    <div
+      class="spinner-border mt-5 text-secondary"
+      style="width: 5rem; height: 5rem"
+      role="status"
+    >
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+  <div class="text-center" v-else>
+    <h3 class="py-3">Welcome to {{ name }}!</h3>
+    <p class="text-dark py-3">Now redirecting you to the home page...</p>
   </div>
 </template>
 
@@ -31,10 +40,12 @@ export default {
     },
     join() {
       let endpoint = `/api/club/${this.query}/membership/`;
-      apiService(endpoint, "POST");
-      setTimeout(() => {
-        this.$router.push({ name: "Home" });
-      }, 3000);
+      apiService(endpoint, "POST").then(() => {
+        this.loading=false;
+        setTimeout(() => {
+          this.$router.push({ name: "Home" });
+        }, 3000);
+      });
     },
   },
   created() {
